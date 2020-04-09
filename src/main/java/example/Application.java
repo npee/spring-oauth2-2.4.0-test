d07a2f0230c5e8fc2da31d1c96aed4186372b8fa
MyeongchanJin <jmcchan@naver.com>
2020-04-09 11:12:21 +0900

    Step 01. 최소한의 보안만 적용한 Web Project 생성

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+}
