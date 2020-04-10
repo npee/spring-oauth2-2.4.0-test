a0e957e641e8b18b3869174587d170db52f491a7
MyeongchanJin <jmcchan@naver.com>
2020-04-10 14:14:12 +0900

    Step 01. 최소한의 보안만 적용한 Web Project 생성 (update)

--- src/main/java/example/controller/HomeController.java
+++ src/main/java/example/controller/HomeController.java
@@ -1,6 +1,5 @@
 package example.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import java.security.Principal;
 
 @RestController
 @RequestMapping("/")
-@Slf4j
 public class HomeController {
 
     @GetMapping
