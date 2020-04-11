1d488089643a1b827c2424e6ddf087f4fc36a074
MyeongchanJin <jmcchan@naver.com>
2020-04-11 18:39:47 +0900

    Step 02. Security 커스터마이징

---
+++ src/main/java/example/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package example.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("npee1").password("{noop}0000").roles("USER")
+                .and()
+                .withUser("npee2").password("{noop}asdf").roles("ADMIN")
+                .and()
+                .withUser("npee3").password("{noop}1234").authorities("ROLE_USER");
+    }
+}
