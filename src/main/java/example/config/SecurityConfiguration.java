dcb991e836d08f51b4ddf74e209ebc769cf29e4a
MyeongchanJin <jmcchan@naver.com>
2020-04-15 19:07:28 +0900

    Step 03. Security 적용되지 않는 페이지 생성(formLogin 수정)

--- src/main/java/example/config/SecurityConfiguration.java
+++ src/main/java/example/config/SecurityConfiguration.java
@@ -27,6 +27,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().formLogin();
     }
+
 }
